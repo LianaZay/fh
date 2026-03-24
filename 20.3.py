3e601b39caed63e5fd3c2e9536b9494f2d0aef09
LianaZay <zaytseva.l@ssmg.cz>
2026-03-24 21:39:50 +0100

    Add files via upload

--- 20.3.py
+++ 20.3.py
@@ -8,19 +8,25 @@ def issimple(number, simplenumbs):
     return True
 
 
-
 def logout(simplenumbs):
     for numb in simplenumbs:
         print(numb)
 
 
+def intofile(simplenumbs, filnam):
+    with open(filnam, "w") as f:
+        for numb in simplenumbs:
+            f.write(str(numb) + "\n")
+
+
 if __name__ == '__main__':
     simplenumbs=[3, 5, 7, 11, 13]
     simplenumbs.append(17)
-    for num in range(19, 100000000, 2):
+    for num in range(19, 100, 2):
         if issimple(num, simplenumbs):
             simplenumbs.append(num)
     logout(simplenumbs)
+    intofile(simplenumbs, "C:\\Users\\Liana\\Desktop\\nmbss.txt")
 
 
 
