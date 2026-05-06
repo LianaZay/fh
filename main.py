a2db645151c75dc0cd36f1e640d0b92feab8868f
LianaZay <zaytseva.l@ssmg.cz>
2026-05-06 10:32:02 +0200

    Clean up main.py by removing blank lines
    Removed unnecessary blank lines and improved code formatting.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import inputnumber
 from inputnumber import request_number
 
-
 def print_hi(name, value):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}. you have entered {value}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -27,34 +26,11 @@ def tobasis(number:int, basis:int):
         result += "-"
     return (result[::-1])
 
-
-
-
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     v = request_number()
     print_hi('Liana', v)
 
-
-
     for r in range(2, 17):
 
         print(f"this number under basis ({r}): ", tobasis(v, r))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
